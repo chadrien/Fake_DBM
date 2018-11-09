4ea6f510c652e6b739fd2079f65f57b8326c66cb
Hadrien de Cuzey <hadrien.de.cuzey@futurice.com>
2018-11-09 15:48:54 +0200

    Make curl requests easier to debug

--- contrib/generate-dbm-versions.sh
+++ contrib/generate-dbm-versions.sh
@@ -7,7 +7,7 @@ tmpDir=$(dirname $0)/tmp
 versionSourceFile=DBM-Core/DBM-Core.lua
 
 mkdir $tmpDir
-curl -sL $latestVersionUrl > $tmpDir/dbm.zip
+curl -L $latestVersionUrl > $tmpDir/dbm.zip
 unzip $tmpDir/dbm.zip $versionSourceFile -d $tmpDir
 
 REVISION=$(cat $tmpDir/$versionSourceFile | grep 'ReleaseRevision =' | grep -o "[0-9][0-9]*")
