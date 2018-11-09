4666c0af97b9ab154edcaf5410c94bb0a8031f17
Hadrien de Cuzey <hadrien.de.cuzey@futurice.com>
2018-11-09 16:13:24 +0200

    Add github auth even to get requests

--- contrib/travis/cron.sh
+++ contrib/travis/cron.sh
@@ -5,9 +5,9 @@ set -ex
 
 contribDir=$(dirname $(dirname $0))
 
-latestFakeDbmVersion=$(curl -L https://api.github.com/repos/chadrien/Fake_DBM/releases/latest | jq -r .tag_name)
+latestFakeDbmVersion=$(curl -u chadrien:${GITHUB_ACCESS_TOKEN} -L https://api.github.com/repos/chadrien/Fake_DBM/releases/latest | jq -r .tag_name)
 
-latestDbmVersionsKnown=$(curl -L https://api.github.com/repos/chadrien/Fake_DBM/contents/dbm_versions.lua?ref=${latestFakeDbmVersion} | jq -r .content)
+latestDbmVersionsKnown=$(curl -u chadrien:${GITHUB_ACCESS_TOKEN} -L https://api.github.com/repos/chadrien/Fake_DBM/contents/dbm_versions.lua?ref=${latestFakeDbmVersion} | jq -r .content)
 $contribDir/generate-dbm-versions.sh
 # the content we get from github is base64 encoded, so let's base64 encode the generated file,
 # as it will be simpler to compare 2 simple strings than 2 multiline strings
